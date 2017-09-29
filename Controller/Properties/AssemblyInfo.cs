fffde4208c69dd693d9d8b2e0093432798fe992a
Rafael Abdala <Rafael_Abdala@hotmail.com>
2017-09-28 22:56:02 -0300

    Estruturacao do projeto. Inicio implementacao login

--- Controller/Properties/AssemblyInfo.cs
+++ Controller/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Model")]
+[assembly: AssemblyTitle("Controller")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Model")]
+[assembly: AssemblyProduct("Controller")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6c794f31-7b55-473f-a815-2216b04abe90")]
+[assembly: Guid("eb2d2185-9395-44a6-b399-989f0a863627")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ Model/Login.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Login
+    {
+        public void buscarUsuario(string cpf, string senha)
+        {
+            // select from usuarios where cpf=cpf 
+
+            //se senha nao for valida, incrementar tentativa
+
+            //se tentativa >= 3, bloqueado = true
+
+            //se senha valida, tentativa =0, bloqueado = false
+
+        }
+    }
+}
